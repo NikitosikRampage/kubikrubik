e97cf1425e2fdb2068d3dc47c7b6bc17240e81e9
NikitosikRampage <nikitadem2006@gmail.com>
2025-03-10 01:10:56 +0300

    jiijijij

---
+++ src/main.c
@@ -0,0 +1,16 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow *window);
+
+// settings
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+int main()
+{
+    glfwInit();
+printf("alo alo");
+}
